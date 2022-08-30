878ee8c1732b38aa13696d92dac96fc1d1834610
Vikas Mishra <engr.vikasmishra@gmail.com>
2022-08-30 17:02:51 +0530

    Added axios call

--- frontend/src/component/EmployeeRegistration.js
+++ frontend/src/component/EmployeeRegistration.js
@@ -1,65 +1,121 @@
-import React, { useState } from "react";
-import { useForm } from 'react-hook-form';
-import { Typography, FormControl, FormControlLabel, Input, InputLabel, Button, Checkbox } from '@mui/material';
-import { useStyles } from '../common/Styles';
+import React, {useState} from "react";
+import {Typography, FormControl, FormControlLabel, Input, InputLabel, Button, Checkbox} from '@mui/material';
+import {useStyles} from '../common/Styles';
 import Alert from '../common/Alert';
+import axios from 'axios';
+
 
 const EmployeeRegistration = () => {
     const classes = useStyles();
     const [showError, setShowError] = useState(false);
-    const { register, handleSubmit } = useForm();
-    
-    const handleUserRegistration = (values) => {
-        setShowError(true);
+    const initialForm = {
+        name: "",
+        empId: "",
+        emailId: "",
+        contactNo: "",
+        teamName: "",
+        supervisorName: "",
+        manager: false
+    }
+
+    const [empForm, setEmpForm] = useState(initialForm);
+
+    const handleInputChange = (e) => {
+        const fieldName= e.target.name
+        const fieldValue = e.target.value
+
+        setEmpForm({
+                ...empForm,
+            [fieldName]: fieldValue
+            }
+        );
+       console.log(empForm)
+    }
+
+    const handleUserRegistration = (e) => {
+        e.preventDefault();
+        console.log("Before",empForm);
+        const inputRequest= JSON.stringify(empForm)
+
+        console.log(inputRequest);
+
+        axios({
+
+            // Endpoint to send files
+            url: "/ims/user/create",
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // Attaching the form data
+            data:inputRequest ,
+        })
+
+            // Handle the response from backend here
+            .then((res) => { console.log("sucesss") })
+
+            // Catch errors if any
+            .catch((err) => {  console.log("failure")});
+
     };
-      
+
     return (
         <>
             <Typography variant="h6" className={classes.title}>
-              Employee Registration
+                Employee Registration
             </Typography>
-            {showError && <Alert />}
-            <form className={classes.form} onSubmit={handleSubmit(handleUserRegistration)}>
-              <FormControl equired margin="normal" fullWidth>
-                <InputLabel htmlFor="name">Name</InputLabel>
-                <Input {...register('name')} id="name" aria-describedby="name" />
-              </FormControl>
+            {showError && <Alert/>}
+            <form className={classes.form} onSubmit={e => handleUserRegistration(e)}>
+                <FormControl required margin="normal" fullWidth>
+                    <InputLabel htmlFor="name">Name</InputLabel>
+                    <Input name="name" aria-describedby="name" onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
-                <InputLabel htmlFor="empId">Employee Id</InputLabel>
-                <Input {...register('empId')}  id="empId" aria-describedby="empId" />
-              </FormControl>
+                <FormControl  margin="normal" fullWidth>
+                    <InputLabel htmlFor="empId">Employee Id</InputLabel>
+                    <Input name="empId" aria-describedby="empId" onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl equired fullWidth>
-                <InputLabel htmlFor="emailId">Email Id</InputLabel>
-                <Input {...register('emailId')}  id="emailId" aria-describedby="emailId" />
-              </FormControl>
+                <FormControl  fullWidth>
+                    <InputLabel htmlFor="emailId">Email Id</InputLabel>
+                    <Input name="emailId" aria-describedby="emailId"
+                           onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
-                <InputLabel htmlFor="contactNo">Contact No</InputLabel>
-                <Input {...register('contactNo')} id="contactNo" aria-describedby="contactNo" />
-              </FormControl>
+                <FormControl margin="normal" fullWidth>
+                    <InputLabel htmlFor="contactNo">Contact No</InputLabel>
+                    <Input name="contactNo" id="contactNo" aria-describedby="contactNo"
+                           onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
-                <InputLabel htmlFor="teamName">Team Name</InputLabel>
-                <Input {...register('teamName')} id="teamName" aria-describedby="teamName" />
-              </FormControl>
+                <FormControl margin="normal" fullWidth>
+                    <InputLabel htmlFor="teamName">Team Name</InputLabel>
+                    <Input name="teamName" id="teamName" aria-describedby="teamName"
+                           onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
-                <InputLabel htmlFor="supervisorName">Supervisor Name</InputLabel>
-                <Input {...register('supervisorName')}  id="supervisorName" aria-describedby="supervisorName" />
-              </FormControl>
+                <FormControl margin="normal" fullWidth>
+                    <InputLabel htmlFor="supervisorName">Supervisor Name</InputLabel>
+                    <Input name="supervisorName" id="supervisorName" aria-describedby="supervisorName"
+                           onChange={handleInputChange}/>
+                </FormControl>
 
-              <FormControl required fullWidth margin="normal">
-                <FormControlLabel {...register('manager')} control={<Checkbox />} label="Is Manager" />
-              </FormControl>
+                <FormControl fullWidth margin="normal">
+                    <FormControlLabel name="manager" control={<Checkbox/>} label="Is Manager"/>
+                </FormControl>
 
-              <Button variant="contained" type="submit" color="primary">
-                Add
-              </Button>
+                <Button variant="contained" type="submit" color="primary">
+                    Add
+                </Button>
+
             </form>
+            <Button href="#" variant="contained" type="submit" color="primary">
+                Reset
+            </Button>
+            <br/>
+
         </>
-      );    
+    );
 }
 
 export default EmployeeRegistration;
--- frontend/src/component/ProductRegistration.js
+++ frontend/src/component/ProductRegistration.js
@@ -20,42 +20,42 @@ const ProductRegistration = () => {
             </Typography>
             {showError && <Alert />}
             <form className={classes.form} onSubmit={handleSubmit(handleProductRegistration)}>
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="productname">Product Name</InputLabel>
                 <Input {...register('productname')} id="productname" aria-describedby="productname" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="description">Description</InputLabel>
                 <Input {...register('description')}  id="description" aria-describedby="description" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="serialNo">Serial Number</InputLabel>
                 <Input {...register('serialNo')} id="serialNo" aria-describedby="serialNo" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="type">Type</InputLabel>
                 <Input {...register('type')}  id="type" aria-describedby="type" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="status">Status</InputLabel>
                 <Input {...register('status')} id="status" aria-describedby="status" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="trackingId">Tracking ID</InputLabel>
                 <Input {...register('trackingId')}  id="trackingId" aria-describedby="trackingId" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="addedBy">Added By</InputLabel>
                 <Input {...register('addedBy')}  id="addedBy" aria-describedby="addedBy" />
               </FormControl>
 
-              <FormControl equired margin="normal" fullWidth>
+              <FormControl required margin="normal" fullWidth>
                 <InputLabel htmlFor="addedTime">Added Time</InputLabel>
                 <Input {...register('addedTime')}  id="addedTime" aria-describedby="addedTime" />
               </FormControl>
